67a9931acaaec926eeeb70c35580986c6cb25b8a
ricamigu <38142658+ricamigu@users.noreply.github.com>
2020-05-22 04:08:59 +0100

    Add files via upload

--- b)/testquadtree.c
+++ b)/testquadtree.c
@@ -10,6 +10,9 @@ int main(){
 	struct node *root;
 	root = new_node();
 
+
+	piece *pec;
+
 	/*
 
 	Coordinate *a = new_coord(5,5);
@@ -32,26 +35,27 @@ int main(){
 	*/
 	CoordinateD partir = new_coordD(20,20);
 
-	Coordinate *a = new_coord(5,15);
+	Coordinate *a = new_coord(10,10);
 	Coordinate *b = new_coord(10,30);
-	Coordinate *c = new_coord(31,10);
+	Coordinate *c = new_coord(30,10);
 	Coordinate *d = new_coord(30,30);
 
 	struct node *leaf1; struct node *leaf2; struct node *leaf3; struct node *leaf4;
-	leaf1 = new_leaf(a); leaf2 = new_leaf(b); leaf3 = new_leaf(c); leaf4 = new_leaf(d);
+	leaf1 = new_leaf(a,pec); leaf2 = new_leaf(b,pec); leaf3 = new_leaf(c,pec); leaf4 = new_leaf(d,pec);
 
 	Coordinate *e = new_coord(1,1);
-	struct node *leaf5; leaf5 = new_leaf(e);
+	struct node *leaf5; leaf5 = new_leaf(e,pec);
 
 	Coordinate *f = new_coord(25,15);
-	struct node *leaf6; leaf6 = new_leaf(f);
+	struct node *leaf6; leaf6 = new_leaf(f,pec);
 
 	Coordinate *centro = new_coord(20,20);
-	struct node *leafcentro; leafcentro = new_leaf(centro);
+	struct node *leafcentro; leafcentro = new_leaf(centro,pec);
 
 	Coordinate *um = new_coord(5,5);
-	struct node *leafum; leafum = new_leaf(um);
+	struct node *leafum; leafum = new_leaf(um,pec);
 
+	/*
 	insert(root, leaf1, partir, 0, 40, 0, 40);
 	insert(root, leaf2, partir, 0, 40, 0, 40);
 	insert(root, leaf3, partir, 0, 40, 0, 40);
@@ -60,23 +64,39 @@ int main(){
 	insert(root, leaf6, partir, 0, 40, 0, 40);
 	insert(root, leafcentro, partir, 0, 40, 0, 40);
 	insert(root, leafum, partir, 0, 40, 0, 40);
+	*/
+
+
+
+	insert2(root, leaf1,20,20);
+	insert2(root, leaf2, 20,20);
+	insert2(root, leaf3, 20,20);
+	insert2(root, leaf4, 20,20);
+	insert2(root, leaf5, 20,20);
+	insert2(root, leaf6, 20,20);
+	insert2(root, leafcentro, 20,20);
+	insert2(root, leafum, 20,20);
+
+
+
+
 
 	//printf("particao: (%f,%f)\n", partir->x,partir->y);
 
 	//inorder(root);
 
-	print_tree(root); printf("\n");
+	//print_tree(root); printf("\n");
 
 	//printf("\nnos: %d\n", number_nodes(root));
-
+	
 	Coordinate *nexiste = new_coord(32,31);
-	struct node *leafnexiste = new_leaf(nexiste);
-
+	/*struct node *leafnexiste = new_leaf(nexiste,pec);
+	*/
 	printf("Contains: ");
 	bool cont=false;
 	//cont = contains(root, leaf5, partir, 0, 40, 0 , 40);
-	//cont = contains2(root, leaf2);
-	cont = containsC(root, 0,0, partir, 0,40,0,40);
+	cont = contains22(root, 32,31,20,20);
+	//cont = containsC(root, 0,0, partir, 0,40,0,40);
 	if(cont==true) printf("exists!");
 
 	/*
